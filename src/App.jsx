a0a8053bfcb2c802aa8dbc7899f5e9f4c0455142
hangmun26 <hangmun260896@gmail.com>
2024-10-06 18:01:16 +0700

    48%

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,86 @@ import Slider from "./slider";
 function App() {
   return (
     <div className="container">
-      <div className="top">0901234567 | hotmail@gmail.com</div>
+      <div className="top">
+        <div className="top_mota">0901234567 | hotmail@gmail.com</div>
+      </div>
 
       <div className="menu">
-        <div className="menu-logo"></div>
-        <ul className="menu-items">
-          <li>Trang chu</li>
-          <li>Gioi thieu</li>
-          <li>linh vuc</li>
-          <li>Du an</li>
-          <li>Tin tuc</li>
-          <li>Tuyen dung</li>
-          <li>Lien he</li>
-          <li className="tim_kiem"></li>
-        </ul>
+        <div className="m_container">
+          <div className="menu-logo">
+            <img
+              style={{
+                width: 60,
+              }}
+              src="https://st2.depositphotos.com/3576231/8284/v/950/depositphotos_82847046-stock-illustration-fragonfly-business-sign-template.jpg"
+              alt="logo"
+            />
+          </div>
+          <ul className="menu-items">
+            <li>Trang chu</li>
+            <li>Gioi thieu</li>
+            <li>linh vuc</li>
+            <li>Du an</li>
+            <li>Tin tuc</li>
+            <li>Tuyen dung</li>
+            <li>Lien he</li>
+            <li className="tim_kiem"></li>
+          </ul>
+        </div>
       </div>
       <div className="overview">
         <div className="ov-container">
           <div className="gioi_thieu">
             <div className="gt-top">
-              <div className="gt-left"></div>
-              <div className="gt-right"></div>
+              <div className="gt-left">
+                <h2>Về chúng tôi</h2>
+                <p>Với tiềm lực ...</p>
+              </div>
+              <div className="gt-right">
+                <h4>
+                  Chúng tôi kiến tạo kiến trúc thương mại & dân cư với độ ngũ
+                  thiết kế chuyên nghiệp
+                </h4>
+                <div className="gtr_list">
+                  <div className="gtr_item">
+                    <div>
+                      <p>01.</p>
+                      <p className="detail">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s
+                      </p>
+                    </div>
+                    <div>
+                      <p>02.</p>
+                      <p className="detail">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s
+                      </p>
+                    </div>
+                  </div>
+                  <div className="gtr_item">
+                    <div>
+                      <p>03.</p>
+                      <p className="detail">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s
+                      </p>
+                    </div>
+                    <div>
+                      <p>04.</p>
+                      <p className="detail">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s
+                      </p>
+                    </div>
+                  </div>
+                  <div className="gtr_second"></div>
+                </div>
+              </div>
             </div>
             <div className="gt-bottom">
               <div className="tam_nhin"></div>
